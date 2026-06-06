7517d7cf1963dac2d6c87df55d807df07d58f2ae
MD-Developer-yt <animeworldday2005@gmail.com>
2026-06-06 19:11:28 +0530

    start.sh

---
+++ start.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Update & upgrade system
+apt update && apt upgrade -y
+
+# ------------------------- #
+# Don't Remove Credit 
+# Ask Doubt @AU_Bot_Discussion 
+# Owner @Mr_Mohammed_29 
+# ------------------------- #
+
+apt install git -y           
+pip install -U pip    
+
+# ------------------------- #
+# Don't Remove Credit 
+# Ask Doubt @AU_Bot_Discussion 
+# Owner @Mr_Mohammed_29 
+# ------------------------- #
+
+# Clone the repo 
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/MD-Developer-yt/MD-Filter-Bot /MD-Filter-Bot
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /Tele-Filter-Bot
+fi
+
+# ------------------------- #
+# Don't Remove Credit 
+# Ask Doubt @AU_Bot_Discussion 
+# Owner @Mr_Mohammed_29 
+# ------------------------- #
+
+# Upgrade pip and install requirements
+cd /Tele-Filter-Bot
+pip install -U -r requirements.txt --force-reinstall
+
+# ------------------------- #
+# Don't Remove Credit 
+# Ask Doubt @AU_Bot_Discussion 
+# Owner @Mr_Mohammed_29 
+# ------------------------- #
+
+# Start bot
+echo "Starting Bot...."
+python3 bot.py
+
+# ------------------------- #
+# Don't Remove Credit 
+# Ask Doubt @AU_Bot_Discussion 
+# Owner @Mr_Mohammed_29 
+# ------------------------- #
