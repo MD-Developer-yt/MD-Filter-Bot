8de12d57e91be75a16077eb5237ba42956104a21
MD-Developer-yt <animeworldday2005@gmail.com>
2026-06-09 10:20:30 +0530

    route.py

---
+++ plugins/route.py
@@ -0,0 +1,25 @@
+
+# ------------------------- #
+# Don't Remove Credit 
+# Ask Doubt @AU_Bot_Discussion 
+# Owner @Mr_Mohammed_29 
+# ------------------------- #
+from aiohttp import web
+
+routes = web.RouteTableDef()
+
+@routes.get("/", allow_head=True)
+
+# ------------------------- #
+# Don't Remove Credit 
+# Ask Doubt @AU_Bot_Discussion 
+# Owner @Mr_Mohammed_29 
+# ------------------------- #
+async def root_route_handler(request):
+    return web.json_response("Bᴏᴛ Dᴇᴘʟᴏʏᴇᴅ, Jᴏɪɴ Us - @Anime_UpdatesAU Oɴ Tᴇʟᴇɢʀᴀᴍ")
+
+# ------------------------- #
+# Don't Remove Credit 
+# Ask Doubt @AU_Bot_Discussion 
+# Owner @Mr_Mohammed_29 
+# ------------------------- #
